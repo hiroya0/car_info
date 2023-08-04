7baa503491ea5d49caeb6b62946a0289566f884d
hiroya0 <2000shigehiro0401@au.com>
2023-08-04 09:35:23 +0900

    add:news_controllerにshowアクションの記述

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,10 +4,19 @@ require 'uri'
 class NewsController < ApplicationController
  def index
    news = News.new(ENV['NEWS_API_KEY'])
-   keywords = ['新車','自動車産業']
+   keywords = ['トヨタ']
    encoded_keywords = keywords.map { |keyword| URI.encode_www_form_component(keyword) }
-   query = encoded_keywords.join(',')
+   query = encoded_keywords.join('OR')
    @news = news.get_everything(language: 'jp', q: query)
+ end
+
+ def show
+   news = News.new(ENV['NEWS_API_KEY'])
+   keywords = ['トヨタ']
+   encoded_keywords = keywords.map { |keyword| URI.encode_www_form_component(keyword) }
+   query = encoded_keywords.join('OR')
+   @news = news.get_everything(language: 'jp', q: query)
+   @comment = Coment.new
  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'homes/index'
+  get 'news/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  resources :news, only: :index
+  resources :homes
+  resources :news
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
