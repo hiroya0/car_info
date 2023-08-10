3cbda5d51dd599627c03177368f1cc96fa6bac01
hiroya0 <2000shigehiro0401@au.com>
2023-08-10 14:49:25 +0900

    fix:idの取得をできるようにする

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,29 +2,22 @@ require 'news-api'
 require 'uri'
 
 class NewsController < ApplicationController
- def index
-   news = News.new(ENV['NEWS_API_KEY'])
-   keywords = ['トヨタ']
-   encoded_keywords = keywords.map { |keyword| URI.encode_www_form_component(keyword) }
-   query = encoded_keywords.join('OR')
-   @article = news.get_everything(language: 'jp', q: query)
-   @article.each do |article_data|
-    article = Article.new(
-      title: article_data.title,
-      url: article_data.url,
-      image_url: article_data.urlToImage
-    )
-    article.save
+    def index
+        news = News.new(ENV['NEWS_API_KEY'])
+        keywords = ['トヨタ']
+        encoded_keywords = keywords.map { |keyword| URI.encode_www_form_component(keyword) }
+        query = encoded_keywords.join('OR')
+        @articles = news.get_everything(language: 'jp', q: query).map do |article|
+            Article.create(
+              title: article.title,
+              url: article.url,
+              image_url: article.urlToImage
+            )
+        end
     end
- end
-
- def show
-   news = News.new(ENV['NEWS_API_KEY'])
-   keywords = ['トヨタ']
-   encoded_keywords = keywords.map { |keyword| URI.encode_www_form_component(keyword) }
-   query = encoded_keywords.join('OR')
-   @article = news.get_everything(language: 'jp', q: query)
-   @comment = Coment.new
- end
+   
+    def show
+      @article = Article.find(params[:id])
+    end
 end
 
