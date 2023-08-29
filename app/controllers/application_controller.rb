6945e63f41d538461977013aa1bb504d3089f2b1
hiroya0 <2000shigehiro0401@au.com>
2023-08-29 23:47:26 +0900

    add:トップ画面にユーザー名を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+ 
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user.id)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
